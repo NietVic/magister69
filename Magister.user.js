b8f2cfc2308de85eedc62177ef4bf40904b17f34
NietVic <96957521+NietVic@users.noreply.github.com>
2023-10-05 16:45:03 +0200

    Update Magister.user.js

--- Magister.user.js
+++ Magister.user.js
@@ -21,11 +21,11 @@
    style.innerHTML = css;
    head.appendChild(style);
    GM_addStyle(GM_getResourceText("style"));
-
-       $("body").append('<div id = "dwj-tamper"><audio autoplay loop> <source src="https://github.com/NietVic/magister69/raw/main/erika.mp3" type="audio/mpeg"></audio></div>');
-    $("body").append(GM_getResourceText("html"));
-
 }
+var erika = document.createElement("div");
+erika.innerHTML = "<audio autoplay loop> <source src='https://github.com/NietVic/magister69/raw/main/erika.mp3' type='audio/mpeg'></audio>";
+document.body.appendChild(erika);
+
 var bg1 = 'https://raw.githubusercontent.com/NietVic/magister69/main/bg1.jpg';
 var bg2 = 'https://raw.githubusercontent.com/NietVic/magister69/main/bg2.jpg';
 var bg3 = 'https://raw.githubusercontent.com/NietVic/magister69/main/bg3.webp';
@@ -43,11 +43,20 @@ var bg7 = 'https://raw.githubusercontent.com/NietVic/magister69/main/bg7.jpg';
 
 
 
-    addGlobalStyle('.bg1{background-image: url('+bg1+')} .bg1_webp{ background-image: url('+bg1+')} .bg2{background-image: url('+bg2+')} .bg2_webp{ background-image: url('+bg2+')} .bg3{background-image: url('+bg3+')} .bg3_webp{ background-image: url('+bg3+')} .bg4{background-image: url('+bg4+')} .bg4_webp{ background-image: url('+bg4+')} .bg5{background-image: url('+bg5+')} .bg5_webp{ background-image: url('+bg5+')} .bg6{background-image: url('+bg6+')} .bg6_webp{ background-image: url('+bg6+')} .bg7{background-image: url('+bg7+')} .bg7_webp{ background-image: url('+bg7+')}')
+    addGlobalStyle('.bg1{background-image: url('+bg1+')} .bg1_webp{ background-image: url('+bg1+')} body{background-color: '+AKleur+'} .bg2{background-image: url('+bg2+')} .bg2_webp{ background-image: url('+bg2+')} .bg3{background-image: url('+bg3+')} .bg3_webp{ background-image: url('+bg3+')} .bg4{background-image: url('+bg4+')} .bg4_webp{ background-image: url('+bg4+')} .bg5{background-image: url('+bg5+')} .bg5_webp{ background-image: url('+bg5+')} .bg6{background-image: url('+bg6+')} .bg6_webp{ background-image: url('+bg6+')} .bg7{background-image: url('+bg7+')} .bg7_webp{ background-image: url('+bg7+')}')
     addGlobalStyle('body{background-color: '+AKleur+';} .k-scheduler{background-color: '+AKleur+'} div.loading-overlay{background-color: '+AKleur+'} :root { --chrome-background: '+AKleur+'; --primary-background: '+AKleur+'; --secondary-background: '+AKleur+'; --background-1: '+AKleur+'; --background-2: '+AKleur+'; --background-3: '+AKleur+'; --background-4: '+AKleur+';--background-5: '+AKleur+'; --background-6: '+AKleur+'; --background-7: '+AKleur+'; --background-8: '+AKleur+'; --background-9: transparent; --background-10: '+AKleur+'; --background-11: '+AKleur+'; --primary-text-color: '+AKleur+'; --secondary-text-color: '+AKleur+';} .menu-footer{background: '+AKleur+';} .ngGrid{background-color: '+AKleur+'} .widget li{background-color: '+AKleur+'} .sm-grid.k-grid .k-grid-content tr{background: '+AKleur+'} .k-grid-content{background: '+AKleur+'} .cijfers-k-grid.k-grid .k-grid-content{background: '+AKleur+'} table.table-grid-layout tr{background: '+AKleur+'} .block .content{background-color: '+AKleur+'} table.table-grid-layout td{color: '+TKleur+' !important;} .widget .list li a{color: '+TKleur+'} #vandaag-container .grade-widget ul{background-color: '+AKleur+'} .block{background-color: '+AKleur+'} .endlink{background-color: '+AKleur+'f} .cijfers-k-grid.k-grid .k-grid-header th.k-header{background: '+AKleur+'} .ngTopPanel{background-color: '+AKleur+'}');
     addGlobalStyle('.sm-grid.k-grid .k-grid-content td{color: '+TKleur+'} table.table-grid-layout > tbody > tr.selected{background-color: '+SKleur+';} .k-scheduler-dayview .k-scheduler-times th, .k-scheduler-weekview .k-scheduler-times th, .k-scheduler-workWeekview .k-scheduler-times th{color: '+TKleur+'} .sm-grid.k-grid .k-grid-content tr.k-state-selected, .sm-grid.k-grid .k-grid-content tr.k-state-selected.k-state-focused, .sm-grid.k-grid .k-grid-content tr.k-state-selected .k-state-focused{background-color:'+SKleur+'} .alert a:hover{background-color:'+SKleur+' !important} .alert{background-color: #213740 !important;}')
     addGlobalStyle('.k-grouping-row td, td.k-group-cell, .k-resize-handle-inner{background-color:'+GRKleur+'}')
 
-})();
 
+/*var P = false;
+    if (!P){
+       alert('h')
+       $("body").append(GM_getResourceText("html"));
+       $("body").append('<div id = "dwj-tamper"><audio autoplay loop> <source src="https://github.com/NietVic/magister69/raw/main/erika.mp3" type="audio/mpeg"></audio></div>');
+       P = true;
+    }*/
+
+
+})();
 
